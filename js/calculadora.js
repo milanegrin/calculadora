c8f1b40dc321cae013418853d093e617c96946f2
Milanegrin <adriana.negrin.17@gmail.com>
2023-05-25 09:47:27 -0500

    maquetacion

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,3 +1,12 @@
-let suma = 1;
-
-console.log(suma);
+let numero
+function tecla1($event) {
+    event.preventDefault();
+    const tecla = $event.target.innerText;
+    console.log(tecla);
+    let cal = document.getElementById("calculo");
+    console.log(cal)
+    if(cal.value == "0"){
+        cal.value = "";
+    }
+    cal.value += tecla;
+}
